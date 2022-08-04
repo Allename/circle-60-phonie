c64b38d935a77a03f2b118161b80d590ec329304
Allename <61892728+Allename@users.noreply.github.com>
2022-08-04 21:59:56 +0000

    Refactored the html and css files

--- app.mjs
+++ app.mjs
@@ -4,13 +4,67 @@ function startApp() {
   // so create and use/call additional functions from here
 
   // pls remove the below and make some magic in here!
-  const inputNumber = document.querySelector('#input-number')
-  const networkLogo = document.querySelector('network_logo')
-  console.log(inputNumber)
+  const networkLogo = document.querySelector(".network-logo");
+  const userNum = document.getElementById("Phone-number");
 
-  if (inputNumber.startsWith('0803' && '0813')) {
-    networkLogo.innerHTML = '<img src="./images/mtn-logo.png" />'
+  userNum.addEventListener("input", getVal);
+
+  const mtnCodes = [
+    "0803",
+    "0806",
+    "0814",
+    "0810",
+    "0813",
+    "0816",
+    "0703",
+    "0903",
+    "0906",
+  ];
+
+  const etisalatCodes = ["0809", "0817", "0818", "0908", "0909"];
+
+  const airtelCodes = [
+    "0802",
+    "0808",
+    "0812",
+    "0708",
+    "0701",
+    "0902",
+    "0901",
+    "0907",
+  ];
+
+  const gloCodes = ["0805", "0807", "0811", "0815", "0705", "0905"];
+
+  function getVal() {
+    for (let i = 0; i < mtnCodes.length; i++) {
+      if (userNum.value.startsWith(mtnCodes[i])) {
+        networkLogo.innerHTML =
+          '<img class="mtn-logo" src="./images/mtn-logo.png" alt="" />';
+      }
+    }
+    for (let i = 0; i < etisalatCodes.length; i++) {
+      if (userNum.value.startsWith(etisalatCodes[i])) {
+        networkLogo.innerHTML =
+          '<img class="etisalat-logo" src="./images/etisalat-logo.png" alt=""/>';
+      }
+    }
+    for (let i = 0; i < airtelCodes.length; i++) {
+      if (userNum.value.startsWith(airtelCodes[i])) {
+        networkLogo.innerHTML =
+          '<img class="airtel-logo" src="./images/airtel-logo.png" alt="" />';
+      }
+    }
+    for (let i = 0; i < gloCodes.length; i++) {
+      if (userNum.value.startsWith(gloCodes[i])) {
+        networkLogo.innerHTML =
+          '<img class="glo-logo" src="./images/glo-logo.jpg" alt="" />';
+      }
+    }
   }
+
+  getVal();
+
 }
 
 // ======= DO NOT EDIT ============== //
